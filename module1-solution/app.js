83034a4fbc725a62e397c77b99fe9c39abb4deaf
Saul Giordani <saulgiordani@MBP-di-Saul.fritz.box>
2016-09-20 01:07:42 +0200

    assignment1

---
+++ module1-solution/app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.msg1 = "Enjoy!";
+  $scope.msg2 = "Too much!";
+  $scope.msg3 = "Please enter data first";
+  $scope.result = "Let's see how hungry you are...";
+
+  $scope.setMsg = function () {
+  	if (!$scope.entireMessage) {
+  		$scope.result = $scope.msg3;
+  	}
+  	else {
+  		var i = $scope.entireMessage.split(",").length;
+
+	   	if (i <= 3) {
+	    	$scope.result = $scope.msg1;
+	  	}
+	  	else {
+	  		$scope.result = $scope.msg2;
+	  	}
+  	}
+  };
+}
+
+})();
